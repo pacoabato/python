a09fccd458ba27ff782a1a386ea207ad2ef63e68
Paco Abato <30805650+pacoabato@users.noreply.github.com>
2019-08-06 08:08:15 +0200

    Update load_siglo_report.py

--- jira_to_google_sheet/load_siglo_report.py
+++ jira_to_google_sheet/load_siglo_report.py
@@ -1,4 +1,20 @@
 # loads data from Jira (via REST API) to Google Sheet report
+# uses Python3
+# python libraries required: gspread oauth2client xlrd
+
+# author: Paco Abato - pacoabato@gmail.com
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 # To work with google sheets
 import gspread
